74b910387299a3089c82e7e66d3030395f31fbb8
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-03-15 18:06:46 +0530

    Updates

--- templates/products-tpl.php
+++ templates/products-tpl.php
@@ -2,23 +2,6 @@
     <h1>Ecopure Warranty Portal</h1>
     <h2>Products</h2>
 
+    <?php
+    ?>
 </div>
-
-<?php
-
-function test($_options) {
-    //default values
-    $default_options = array(
-        'name' => 'Name Not Set ',
-        'value' => 'Value Empty'
-    );
-    is_array($_options) ? $options = array_merge($default_options, $_options) : $options = $default_options;
-    echo 'Name: ' . $options['name'] . '<br>';
-    echo 'Value: ' . $options['value'];
-}
-
-//$product['name'] = 'Product1';
-$product['value'] = 154;
-//$product = 'Test';
-test($product);
-?>
